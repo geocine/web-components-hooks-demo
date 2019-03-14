1bb8d33c036ccb058a6a4aef0b53b2d2ae36cc10
Aivan Monceller <aivan@monceller.com>
2019-03-14 21:30:22 +0800

    feat(cart): update cart page style

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,16 +2,47 @@ import { html } from "lit-html";
 import { css } from "goober";
 
 const CartClass = css`
-  background: dodgerblue;
-  color: white;
-  padding: 1em;
+  width: 50rem;
+  max-width: 90%;
+  margin: 2rem auto;
+
+  p {
+    text-align: center;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    padding: 1rem;
+    margin: 1rem 0;
+    border: 1px solid #00179b;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
 const CartPage = () => {
   return html`
-    <div class=${CartClass}>Cart Page</div>
-    <router-link to="/">Home</router-link>
+    <main-navigation></main-navigation>
+    <main class="${CartClass}">
+      <ul>
+          <li>
+            <div>
+              <strong>Title</strong> - 100 (2)
+            </div>
+            <div>
+              <button>
+                Remove from Cart
+              </button>
+            </div>
+          </li>
+      </ul>
+    </main>
   `;
-}
+};
 
-export default CartPage
+export default CartPage;
